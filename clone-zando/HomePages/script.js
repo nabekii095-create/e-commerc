8d9cf82771f7de2d4b083b3c8f86d227352bf73f
OrnKimhak <hak6967@gmail.com>
2026-07-07 15:23:10 +0700

    update detail product

---
+++ clone-zando/HomePages/script.js
@@ -0,0 +1,16 @@
+// const slideImage = document.querySelector(".slide-image");
+// const images = document.querySelectorAll(".slide-image img");
+
+// let currentIndex = 0;
+
+// function showNextImage() {
+//     currentIndex++;
+
+//     if (currentIndex >= images.length) {
+//         currentIndex = 0;
+//     }
+
+//     slideImage.style.transform = `translateX(-${currentIndex * 100}%)`;
+// }
+
+// setInterval(showNextImage, 3000);
