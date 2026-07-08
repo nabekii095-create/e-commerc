2d936546d0668d1100b7de31306543695076d64d
ornkimhak <hak6967@gmail.com>
2026-07-08 20:06:48 +0700

    update all page

--- clone-zando/HomePages/script.js
+++ clone-zando/HomePages/script.js
@@ -1,16 +1,16 @@
-// const slideImage = document.querySelector(".slide-image");
-// const images = document.querySelectorAll(".slide-image img");
+const slideImage = document.querySelector(".slide-image");
+const images = document.querySelectorAll(".slide-image img");
 
-// let currentIndex = 0;
+let currentIndex = 0;
 
-// function showNextImage() {
-//     currentIndex++;
+function showNextImage() {
+    currentIndex++;
 
-//     if (currentIndex >= images.length) {
-//         currentIndex = 0;
-//     }
+    if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
 
-//     slideImage.style.transform = `translateX(-${currentIndex * 100}%)`;
-// }
+    slideImage.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
 
-// setInterval(showNextImage, 3000);
+setInterval(showNextImage, 3000);
